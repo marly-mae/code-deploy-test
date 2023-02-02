f0872b6ae98ff2c111bfc51a5001717a66c24b6e
marly-mae <108348008+marly-mae@users.noreply.github.com>
2023-02-02 09:04:39 +0800

    added delete agent v4

--- update-agent.bat
+++ update-agent.bat
@@ -1,3 +1,4 @@
 sc delete jambo-agent-v4 
+sc delete jambo-agent
 sc create jambo-agent binPath= "C:\Users\Administrator\Downloads\agent-service.exe http://172.31.21.47:8085"
 sc start auto jambo-agent
