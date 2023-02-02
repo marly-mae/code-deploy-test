1c23e2b19b2c4a0878847b94eaa7c63ec49a46ea
marly-mae <108348008+marly-mae@users.noreply.github.com>
2023-02-02 09:23:24 +0800

    added if else for win7

--- update-agent.bat
+++ update-agent.bat
@@ -1,4 +1,5 @@
+sc stop jambo-agent-v4 
 sc delete jambo-agent-v4 
 sc delete jambo-agent
-sc create jambo-agent binPath= "C:\Users\Administrator\Downloads\agent-service.exe http://172.31.21.47:8085" start=auto
+if %COMPUTERNAME%==WINDOWS7CLIENT (sc \\localhost create jambo-agent binPath= "C:\Users\Administrator\Downloads\agent-service.exe http://172.31.21.47:8085" start=auto) else (sc create jambo-agent binPath= "C:\Users\Administrator\Downloads\agent-service.exe http://172.31.21.47:8085" start=auto)
 sc start jambo-agent
